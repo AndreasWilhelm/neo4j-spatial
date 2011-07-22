879c027f6ad290c930cbc90b34d6e7e66b19e9ae
AndreasWilhelm <apw.desire2@googlemail.com>
2011-07-22 16:00:50 +0200

    ADDED: Comments

--- src/main/java/org/neo4j/gis/spatial/query/geometry/editors/ST_Union.java
+++ src/main/java/org/neo4j/gis/spatial/query/geometry/editors/ST_Union.java
@@ -30,22 +30,51 @@ import org.neo4j.gis.spatial.operation.AbstractFullOperation;
 import org.neo4j.gis.spatial.operation.OperationType;
 import org.neo4j.gis.spatial.operation.SpatialTypeOperation;
 import org.neo4j.graphdb.Node;
+
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 
 /**
+ * <p>
+ * The <code>ST_Union</code> class returns a {@link Geometry} which represents
+ * all points of the node geometry and the other geometry.
+ * </p>
+ * 
+ * <h3>For example:</h3>
+ * <ul>
+ * <li>Input: LINESTRING (12.9710302 56.0538436, 12.9726158 56.0546985,
+ * 12.9726773 56.0547317, 12.9735859 56.0552154, 12.9738426 56.0553521,
+ * 12.9747403 56.0559176, 12.9757125 56.056313, 12.9759293 56.0564416,
+ * 12.9760919 56.0567821, 12.9761463 56.0568715, 12.9763358 56.057183,
+ * 12.9763358 56.0575008, 12.9763764 56.0577353, 12.9762985 56.0581325,
+ * 12.9762427 56.058262, 12.9762034 56.0583531)</li>
+ * <li>Output: MULTILINESTRING ((12.9710302 56.0538436, 12.9726158 56.0546985,
+ * 12.9726773 56.0547317, 12.9735859 56.0552154, 12.9738426 56.0553521,
+ * 12.9747403 56.0559176, 12.9757125 56.056313, 12.9759293 56.0564416,
+ * 12.9760919 56.0567821, 12.9761463 56.0568715, 12.9763358 56.057183,
+ * 12.9763358 56.0575008, 12.9763764 56.0577353, 12.9762985 56.0581325,
+ * 12.9762427 56.058262, 12.9762034 56.0583531), (13.9639158 56.070904,
+ * 13.9639658 56.0710206, 13.9654342 56.0711966))</li>
+ * </ul>
+ * <p>
  * 
  * @author Andreas Wilhelm
  * 
  */
 public class ST_Union extends AbstractFullOperation {
-	
+
+	// The geometry from which to union with the geometry from the query.
 	private Geometry other;
-	
+
 	/**
+	 * Construct a {@link Geometry} which represents all points of the node
+	 * geometry and the provided other geometry.
 	 * 
 	 * @param other
+	 *            the geometry from which to union with the geometry from the
+	 *            query.
+	 * 
 	 */
 	public ST_Union(Geometry other) {
 		this.other = other;
@@ -58,21 +87,19 @@ public class ST_Union extends AbstractFullOperation {
 	public SpatialDatabaseRecord onIndexReference(OperationType type,
 			Node node, Layer layer, List<SpatialDatabaseRecord> records) {
 		SpatialDatabaseRecord record = null;
-	    GeometryFactory geometryFactory = new GeometryFactory();
-	    Collection<Geometry> geometries = new ArrayList<Geometry>();
-	    			    		
+		GeometryFactory geometryFactory = new GeometryFactory();
+		Collection<Geometry> geometries = new ArrayList<Geometry>();
+
 		Geometry geom = this.decodeGeometry(node);
 		geometries.add(geom);
 		geometries.add(other);
-		
-	    GeometryCollection geometryCollection =
-	            (GeometryCollection) geometryFactory.buildGeometry( geometries );
 
-		
+		GeometryCollection geometryCollection = (GeometryCollection) geometryFactory
+				.buildGeometry(geometries);
+
 		Geometry unionGeom = geometryCollection.union();
-		
-		record = new SpatialDatabaseRecordImpl(
-				layer, node, unionGeom);
+
+		record = new SpatialDatabaseRecordImpl(layer, node, unionGeom);
 		record.setResult(unionGeom);
 		records.add(record);
 		return record;
